1211883fae8e6b3964690a4c038c6ba588eb100e
rigole <foplacide@gmail.com>
2022-11-27 08:04:58 +0100

    resseting the frontend for accesibity

---
+++ frontend/src/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello Here is the the tester to test tailwindcss
+            </h1>
+        </div>
+    )
+}
+
+export default Header
