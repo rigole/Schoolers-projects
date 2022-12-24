9462acae25f63abdd718c0eb79bac537b856c8f4
rigole <foplacide@gmail.com>
2022-12-24 18:34:38 +0100

    media queries for examinations and changing images

--- frontend/src/Examination/Examination.tsx
+++ frontend/src/Examination/Examination.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import OrientationImage from "../images/orientation.jpg";
+import OrientationImage from "../images/orientation.jpg"
 
 const Examination = () => {
 
     return (
         <section className="bg-gray-200 w-full ">
-        <div className="flex flex-row items-center justify-center px-45">
-            <div className="items-start">
-                <img  src={OrientationImage} alt="" className="w-[350px] h-[350px] rounded-full items-center text-center py-4"/>
-            </div>
-           
+            <div className="choices flex flex-row items-center justify-center py-4 px-2">
+                <div className="items-start">
+                    <img  src={OrientationImage} alt="" className="w-[350px] h-[350px] rounded-full items-center text-center py-4"/>
+                </div>
                 <div className="flex flex-col items-center justify-center">
                     <div className="">
-                        <p className="text-center">Don't know where to start? Think about taking an orientation test <br/>to know in which field of study you could succeed</p>
+                        <p className="text-center">You can also get all the informations about all examination entrance<br/>  of some schools either privates or publics schools</p>
                     </div>
                     <a className="space-x-5  rounded-md bg-transparent text-white  hover:text-white transition-all" href="">
-                        <button className="rounded-full mt-4 py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Take the test</button>
+                        <button className="rounded-full mt-4 py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Get informations</button>
                     </a>
-                </div>
+                </div>        
             </div>
-            
-         
         </section>
     )
 }
--- frontend/src/Orientation/Orientation.tsx
+++ frontend/src/Orientation/Orientation.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import OrientationImage from "../images/orientation.jpg"
+import TestImage from "../images/test.jpg";
+
 
 const Orientation = () => {
 
     return(
         <section className="flex flex-col text-center items-center bg-gray-100 py-4">
             <div>
-                <img  src={OrientationImage} alt="" className="w-[350px] h-[350px] rounded-full items-center text-center py-4"/>
+                <img  src={TestImage} alt="" className="w-[350px] h-[350px] rounded-full items-center text-center py-4"/>
             </div>
             <div className="px-74">
                 <p className="">Don't know where to start ? Think about taking an orientation test <br/>to know in which field of study you could succeed</p>
