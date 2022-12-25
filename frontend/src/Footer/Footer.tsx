a13145fc7789974c03791754a1ff59b829f93813
rigole <foplacide@gmail.com>
2022-12-25 10:47:10 +0100

    adding social media icon

--- frontend/src/Footer/Footer.tsx
+++ frontend/src/Footer/Footer.tsx
@@ -42,6 +42,33 @@ const Footer = () => {
                 <p>About us</p>
                 <p>About us</p>
            </div>
+
+           <div >
+                <h3>GET IN TOUCH</h3>
+                <div className="flex flex-row justify-between">
+                <p>
+                    <a href="#">
+                         <i className="fab fa-facebook-f "></i>
+                    </a>
+                </p>
+                <p>
+                <a href="#">
+                         <i className="fab fa-twitter"></i>
+                    </a>
+                </p>
+                <p>
+                <a href="#">
+                         <i className="fab fa-linkedin"></i>
+                    </a>
+                </p>
+                <p>
+                <a href="#">
+                         <i className="fab fa-instagram"></i>
+                    </a>
+                </p>
+                </div>
+                
+           </div>
         </footer>
     )
 } 
