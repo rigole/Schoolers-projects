70cd1e87f4a19ba4792fc10595d04e15eb27474d
rigole <foplacide@gmail.com>
2023-01-14 19:35:04 +0100

    loader attempt but not success

--- frontend/src/Pages/Universities.tsx
+++ frontend/src/Pages/Universities.tsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { schoolList } from "../actions/schoolActions";
-import OrientationImage from "../images/orientation.jpg"
+import OrientationImage from "../images/orientation.jpg";
+import Loader from "../components/Loader/Loader"
 import IucImage from "../images/iuc.png"
 
 function Universities() {
 
     const dispatch = useDispatch();
     const school_list = useSelector((state:any) => state.schoolList);
-    const { schools } = school_list;
+    const { schools, loading, error } = school_list;
 
     useEffect(() => {
         dispatch<any>(schoolList())
@@ -52,42 +53,41 @@ function Universities() {
                 </div>
             </div>
             <div className="flex school_list">
-                {schools.map(school => (
-                <div className="relative university_singe_card w-[350px] h-[590px] shadow-[0_30px_30px_0_rgba(0,0,0,0.5)] rounded-2xl my-9 ml-4">
-                    <div className="h-[40%] w-full">
-                        <img src={OrientationImage} className="rounded-t-2xl" alt="" />
-
-                            <div className="pp_university bg-white h-[75px] w-[75px] shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]  rounded-2xl">
-                                <img src={IucImage} className="rounded-full h-full w-full" alt="" />
+                {
+                    schools.map(school => (
+                        <div className="relative university_singe_card w-[350px] h-[590px] shadow-[0_30px_30px_0_rgba(0,0,0,0.5)] rounded-2xl my-9 ml-4">
+                            <div className="h-[40%] w-full">
+                                <img src={OrientationImage} className="rounded-t-2xl" alt="" />
+        
+                                    <div className="pp_university bg-white h-[75px] w-[75px] shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]  rounded-2xl">
+                                        <img src={IucImage} className="rounded-full h-full w-full" alt="" />
+                                    </div>
+                                    <div className="like_university h-[45px] w-[45px] px-1 py-1 shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]">
+                                        <i className="fa-regular fa-thumbs-up fa-2x"></i>
+                                    </div>
+                            <div className="school_description">
+                                <h1 className="text-center text-2xl font-bold mt-2">{school.name}</h1>
+        
+                                <div className="location">
+                                    <span> <i className="fa-sharp fa-solid fa-location-pin"></i>Douala</span>
+                                    <span><i className="fa-solid fa-school"></i> private</span>
+                                </div>
+                                <p className="text-center m-4">
+                                   {school.description}
+                                </p>
                             </div>
-                            <div className="like_university h-[45px] w-[45px] px-1 py-1 shadow-[0_5px_5px_0_rgba(0,0,0,0.5)]">
-                                <i className="fa-regular fa-thumbs-up fa-2x"></i>
-                            </div>
-                    <div className="school_description">
-                        <h1 className="text-center text-2xl font-bold mt-2">{school.name}</h1>
-
-                        <div className="location">
-                            <span> <i className="fa-sharp fa-solid fa-location-pin"></i>Douala</span>
-                            <span><i className="fa-solid fa-school"></i> private</span>
+                            <Link to="/universities" className="flex justify-center m-4 rounded-md bg-transparent text-white  hover:text-white transition-all">
+                                <button className=" rounded-full  py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Read more</button>
+                            </Link>
                         </div>
-                        <p className="text-center m-4">
-                           {school.description}
-                        </p>
-                    </div>
-                    <Link to="/universities" className="flex justify-center m-4 rounded-md bg-transparent text-white  hover:text-white transition-all">
-                        <button className=" rounded-full  py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Read more</button>
-                    </Link>
-                </div>
-               
+                      </div>
+                    ))} 
                 </div>
 
-               
-))}
-            </div>
-
-                
+              
         </section>
-    )
-}
 
+    )
+
+}
 export default Universities
