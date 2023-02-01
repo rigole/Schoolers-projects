3153bfd05b5e4c2ee7d590a229f76b0bf616cf52
rigole <foplacide@gmail.com>
2023-02-01 20:51:19 +0100

    actions for the field of study + urls and views in the backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schools/', include('schools.urls.school_urls')),
+    path('api/fieldofstudy/', include('schools.urls.fields_of_study_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
