dfd221516c057acc2d162da45c322fe06b6b5e75
rigole <foplacide@gmail.com>
2023-04-26 10:39:11 +0100

    setting the unhandled route with fallback compoenent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
@@ -8,19 +9,29 @@ import Universities from './Pages/Universities';
 import SchoolDetails from './components/SchoolDetails/SchoolDetails';
 import Programs from './Pages/Programs';
 import FieldStudy from './Pages/FieldStudy';
+import FallbackRender from './components/Error-Boundary/Error';
+
+
 
 function App() {
   return (
     <div className="App">
+      
         <Header/>
+       
         <Routes>
-          <Route  path='/' element={<HomePage/>} />
-          <Route path='/universities' element={<Universities/>}/>
-          <Route path='/programs' element={<Programs/>}/>
-          <Route path='/universities/:school_id' element={<SchoolDetails/>}/>
-          <Route path='/fields' element={<FieldStudy/>}/>
+         
+            <Route  path='/' element={<HomePage/>} />
+            <Route path='/universities' element={<Universities/>}/>
+            <Route path='/programs' element={<Programs/>}/>
+            <Route path='/universities/:school_id' element={<SchoolDetails/>}/>
+            <Route path='/fields' element={<FieldStudy/>}/>
+            <Route path='*' element={<FallbackRender/>}/>
+        
         </Routes>
+       
         <Footer/>
+       
 
     </div>
   );
