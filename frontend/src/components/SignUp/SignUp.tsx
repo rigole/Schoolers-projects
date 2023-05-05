da50d710de2568fc69e6cebe9cd8633da0da719a
rigole <foplacide@gmail.com>
2023-05-05 12:12:53 +0100

    SETTING THE FORM OF SIGN UP WITH CSS

--- frontend/src/components/SignUp/SignUp.tsx
+++ frontend/src/components/SignUp/SignUp.tsx
@@ -1,40 +1,32 @@
 import { Link } from "react-router-dom"
 import Button from "../Button/Button"
+import OrientationImage from "../../images/orientation.jpg";
 
 
 function SignUp () {
 
     return (
-        <div>
-             <form id="stripe-login">
-                    <div className="field padding-bottom--24">
-                      <label htmlFor="email">Email</label>
-                      <input type="email" name="email"/>
-                    </div>
-                    <div className="field padding-bottom--24">
-                      <div className="grid--50-50">
-                        <label htmlFor="password">Password</label>
-                      </div>
-                      <input type="password" name="password"/>
-                    </div>
-                    <div className="field field-checkbox padding-bottom--24 flex-flex align-center text-black">
-                      <label htmlFor="checkbox">
-                        Don't yet have an account? <Link to="/signup">   Sign Up</Link>
-                      </label>
-                    </div>
-                    <div className="field padding-bottom--24 text-white w-[345px] text-center">
-                     <Button text="Sign In " paddingX={14} paddingY={2} marginTop={4} />
-                    </div>
-                    <div className="field">
-                      <a className="ssolink" href="#"></a>
-                      <p>
-                        <a href="#">
-                            <i className="fab fa-google "></i>Sign in with Google
-                        </a>
-                      </p>
-                    </div>
-                  </form>
-        </div>
+        <section className="signUpSection pt-20">
+          <h1 className="text-3xl text-center pt-10 font-bold">SIGN UP FORM</h1>
+          <div className="signUpElements">
+            <div className="items-start">
+              <img  src={OrientationImage} alt="" className="w-[350px] h-[350px] rounded-full items-center text-center py-4"/>
+            </div>
+            <div>
+              <form className="signUpForm" action="">
+                <label htmlFor="">Name</label>
+                <input type="text" className="w-[450px] h-[50px] border-solid border-2 rounded-full py-3" required />
+                <label htmlFor="">Email</label>
+                <input type="email"  className="w-[450px] h-[50px] border-solid border-2 rounded-full" required />
+                <label htmlFor="">Password</label>
+                <input type="password"  className="w-[450px] h-[50px] border-solid border-2 rounded-full mb-2" required />
+                <Button marginTop={7} text="Sign Up" paddingX={14} paddingY={2} />
+              </form>
+            </div>
+          </div>
+        
+          
+        </section>
     )
 }
 
