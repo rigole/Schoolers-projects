55780e304cc2f7b6b9c031cf1623d22628fc3b55
rigole <foplacide@gmail.com>
2023-06-14 08:13:07 +0100

    setting the filter bug on field of study, programs

--- frontend/src/Pages/Programs.tsx
+++ frontend/src/Pages/Programs.tsx
@@ -5,23 +5,13 @@ import {Link} from "react-router-dom";
 import { programList } from "../actions/programsActions";
 
 const Programs = () => {
-    const [input, setInput] = useState('')
+    const [programInput, setProgramInput] = useState("")
 
     const dispatch = useDispatch()
     const programs_list = useSelector((state:any) => state.programState);
 
     const { programs, loading, error } = programs_list;
 
-
-    const [programsFiltered, setProgramsFiltered] = useState(programs)
-
-    const filteredSchools = async (input) => {
-        const filtered =  programs.filter(program => {
-            return program.name.toLowerCase().includes(input.toLowerCase())
-        })
-        setInput(input);
-        setProgramsFiltered(filtered);
-    }
     console.log(programs)
 
     useEffect(() =>{
@@ -35,7 +25,7 @@ const Programs = () => {
             </div>
             <div className="text-center" >
                 <form className="px-0 py-0">
-                    <input value={input} onChange={(e => filteredSchools(e.target.value))} className="nosubmit w-1/2 h-9 rounded-xl" type="search" placeholder="Search Programs" required/>
+                    <input value={programInput} onChange={(e => setProgramInput(e.target.value))} className="nosubmit w-1/2 h-9 rounded-xl" type="search" placeholder="Search Programs" required/>
                 </form>
             </div>
 
@@ -45,25 +35,29 @@ const Programs = () => {
                         ? <h1>Error</h1>
                     :
                         (
-
-                            programsFiltered.map(program => (
-
-                                <div className="program_card">
-                                    <div className="imgbox">
-                                        <img className="program_image w-full h-full object-cover" src={`http://127.0.0.1:8000${program.image}`} alt=""/>
+                            programs.map(program => {
+                                if(programInput == "" || program.name.toLowerCase().includes(programInput.toLowerCase()) ){
+                                    return (
+                                        <div className="program_card">
+                                        <div className="imgbox">
+                                            <img className="program_image w-full h-full object-cover" src={`http://127.0.0.1:8000${program.image}`} alt=""/>
+                                        </div>
+                                        <div className="program_content">
+                                            <h3 className="text-xl uppercase font-bold m-2">{program.name}</h3>
+                                            <p className="program_description text-black">
+                                                {program.description}
+                                            </p>
+                                            <Link to={`/programs/${program.program_id}`} className="readmore_program flex justify-center rounded-md bg-transparent text-white  hover:text-white transition-all">
+                                                <button className=" rounded-full  py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Read more</button>
+                                            </Link>
+                                        </div>
                                     </div>
-                                    <div className="program_content">
-                                        <h3 className="text-xl uppercase font-bold m-2">{program.name}</h3>
-                                        <p className="program_description text-black">
-                                            {program.description}
-                                        </p>
-                                        <Link to={`/programs/${program.program_id}`} className="readmore_program flex justify-center rounded-md bg-transparent text-white  hover:text-white transition-all">
-                                            <button className=" rounded-full  py-2 px-14 border-1 bg-amber-500 hover:bg-amber-300">Read more</button>
-                                        </Link>
-                                    </div>
-                                </div>
-
-                            ))
+                                    )
+                                }
+                            }
+                                
+                                
+                            )
                         )
                 }
             </div>
